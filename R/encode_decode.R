ce4805e131657c1bc76e826712ec530e34833239
rdinnager <r.dinnage@gmail.com>
2023-11-20 11:29:01 -0500

    Added support for terra::SpatRaster inputs and outputs to encoder and decoder functions.

--- R/encode_decode.R
+++ R/encode_decode.R
@@ -1,18 +1,25 @@
 #' Encode bioclim data to biocman data (5 bioclim manifold variables)
 #'
-#' @param x A matrix or data.frame. Columns are assumed to be in order from
-#' BIO01 to BIO19
+#' @param x A matrix, data.frame, or SpatRaster. Columns are assumed to be in order from
+#' BIO01 to BIO19. Result will be incorrect if they are not.
 #' @param standardised Are the bioclim variables standardised? If so, this argument
 #' should be `TRUE` and they must have been standardised using the mean and SD of the
 #' WorldClim 2.1 2.5 minute data. These values can be found in `bioclim_scaling`
 #' @param device A [torch::torch_device()], on which to run the encoder model, default: `"cpu"`
 #'
 #' @return A matrix of biocman variables (5 bioclimatic manifold variables). Rows
-#' correspond to row in `x`
+#' correspond to rows in `x`. If `x` is a `SpatRaster`, the result will also be a
+#' `SpatRaster`.
 #' @export
 biocman_encode <- function(x, standardised = FALSE, device = "cpu") {
 
-  dat_processed <- x
+  if(inherits(x, "SpatRaster")) {
+    df <- terra::as.data.frame(x, xy = TRUE, na.rm = FALSE)
+    dat_processed <- as.matrix(df[ , -which(colnames(df) %in% c("x", "y"))])
+    spatraster <- TRUE
+  } else {
+    dat_processed <- x
+  }
   dat_processed <- dat_processed[ , c(1, 10, 11, 12, 13, 14,
                                       15, 16, 17, 18, 19, 2,
                                       3, 4, 5, 6, 7, 8, 9)]
@@ -27,23 +34,44 @@ biocman_encode <- function(x, standardised = FALSE, device = "cpu") {
   bioman <- as.matrix(mod(c_null, torch::torch_tensor(dat_processed, device = "device"))$means$cpu())
   bioman <- bioman[ , -non_manifold_dim_nums]
 
+  colnames(bioman) <- c("BIOMAN1", "BIOMAN2", "BIOMAN3", "BIOMAN4", "BIOMAN5")
+
+  if(spatraster) {
+    bioman <- terra::rast(cbind(as.matrix(df[ , c("x", "y")]), bioman),
+                          type = "xyz",
+                          crs = terra::crs(x),
+                          extent = terra::ext(x))
+  }
+
+  bioman
+
 }
 
 #' Decode biocman data to bioclim data (19 bioclimatic variables)
 #'
-#' @param x A matrix or data.frame. Columns are assumed to be in order from
-#' BIOMAN1 to BIOMAN5
+#' @param x A matrix, data.frame, or SpatRaster. Columns are assumed to be in order from
+#' BIOMAN1 to BIOMAN5.  Result will be incorrect if they are not.
 #' @param standardised Should the returned bioclim variables be in standardised
 #' form (the default)? If `FALSE` the variables will be back-transformed to the
 #' original scale of the bioclim variables
 #' @param device A [torch::torch_device()], on which to run the decoder model, default: `"cpu"`
 #'
 #' @return A matrix of bioclim variables (19 bioclimatic variables). Rows
-#' correspond to row in `x`.
+#' correspond to rows in `x`. If `x` is a `SpatRaster`, the result will also be a
+#' `SpatRaster`.
 #' @export
 biocman_decode <- function(x, standardised = TRUE, device = "cpu") {
-  dat_processed <- matrix(0, ncol = 16, nrow = nrow(x))
-  dat_processed[ , -non_manifold_dim_nums] <- as.matrix(x)
+
+  if(inherits(x, "SpatRaster")) {
+    df <- terra::as.data.frame(x, xy = TRUE, na.rm = FALSE)
+    mat <- as.matrix(df[ , -which(colnames(df) %in% c("x", "y"))])
+    spatraster <- TRUE
+  } else {
+    mat <- as.matrix(x)
+  }
+
+  dat_processed <- matrix(0, ncol = 16, nrow = nrow(mat))
+  dat_processed[ , -non_manifold_dim_nums] <- mat
 
   mod <- biocman_env$biocman_vae$decoder$to(device = device)
 
@@ -60,6 +88,14 @@ biocman_decode <- function(x, standardised = TRUE, device = "cpu") {
                         "BIO03", "BIO04", "BIO05", "BIO06", "BIO07", "BIO08",
                         "BIO09")
   bioman <- bioman[ , order(colnames(bioman))]
+
+  if(spatraster) {
+    bioman <- terra::rast(cbind(as.matrix(df[ , c("x", "y")]), bioman),
+                          type = "xyz",
+                          crs = terra::crs(x),
+                          extent = terra::ext(x))
+  }
+
   bioman
 
 }
